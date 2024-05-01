1e8194930228a6ec1d6699390cadf37d1dbb068b
Naihtar <sltrpa.no@gmail.com>
2024-05-01 15:42:16 +0200

    (XAM) - TFG:
     - Diseño final de la ventana principal.

--- ViewModels/WorkSpaceViewModel.cs
+++ ViewModels/WorkSpaceViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using TFG.Services.DatabaseServices;
 using TFG.ViewModels.Base;
 using TFGDesktopApp.Models;
 
@@ -6,6 +7,7 @@ namespace TFG.ViewModels {
     public class WorkSpaceViewModel : BaseViewModel {
         private User _user;
         private MainWindowViewModel _mainWindowViewModel;
+        private readonly DatabaseService _databaseService;
 
         public ObservableCollection<string> NombreContenedores { get; }
 
@@ -17,12 +19,22 @@ namespace TFG.ViewModels {
             NombreContenedores = new ObservableCollection<string>();
             _user = user;
             _mainWindowViewModel = mainWindowViewModel;
+            _databaseService = new DatabaseService();
             MyCommand = new CommandViewModel(ExecuteMyCommand);
+            _ = FillContainerNames();
+
         }
 
         private void ExecuteMyCommand(object parameter) {
             // Cambia IsValueTrue a true cuando se ejecuta el comando.
             _mainWindowViewModel.IsValueTrue = true;
+        }
+
+        public async Task FillContainerNames() {
+            foreach (var containerId in _user.ListaContenedoresUsuario) {
+                var containerName = await _databaseService.GetContainerByIdAsync(containerId);
+                NombreContenedores.Add(containerName.NombreContenedor);
+            }
         }
     }
 }
