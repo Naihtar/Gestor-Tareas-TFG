d7c2de0c82c63aa09ba3880334ba821e91f81a0a
Naihtar <sltrpa.no@gmail.com>
2024-05-01 15:43:49 +0200

    (---) LoginPage.xaml: Cambios menores de diseño.

--- Views/Pages/LoginPage.xaml.cs
+++ Views/Pages/LoginPage.xaml.cs
@@ -5,13 +5,11 @@ using TFG.ViewModels;
 
 namespace TFG.Views.Pages {
     public partial class LoginPage : Page {
-        private readonly LoginViewModel _loginViewModel;
 
         public LoginPage(Frame mainFrame, MainWindowViewModel mainWindowViewModel) {
             InitializeComponent();
             INavigationService navigationService = new NavigationService(mainFrame);
-            _loginViewModel = new LoginViewModel(navigationService, mainWindowViewModel);
-            DataContext = _loginViewModel;
+            DataContext = new LoginViewModel(navigationService, mainWindowViewModel);
         }
     }
 }
