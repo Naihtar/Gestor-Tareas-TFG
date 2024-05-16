28719d0c1a50cb7c4a1a727766ea2792b43c816d
Naihtar <sltrpa.no@gmail.com>
2024-05-16 13:18:46 +0200

    (CSP) - Services/NavigationService: Implementación de interfaz.
     - Siguiendo los principios SOLID, he implementado una interfaz para gestionar las dependencias de los servicios de autentificación.
     - Implementación de la interfaz INavigationService.
     - Reestructuración del servicio.
     - Añadidos nuevos parametros al los métodos del servicio.

--- Services/NavigationServices/INavigationService.cs
+++ Services/NavigationServices/INavigationService.cs
@@ -1,11 +1,17 @@
-﻿using TFG.ViewModels;
+﻿using System.ComponentModel;
+using TFG.Models;
+using TFG.Services.AuthentificationServices;
+using TFG.Services.DatabaseServices;
 using TFGDesktopApp.Models;
 
 namespace TFG.Services.NavigationServices {
 
     public interface INavigationService {
-        void NavigateToLogin();
-        void NavigateTo(string route, AppUser user, INavigationService nav);
+        void NavigateTo(IDatabaseService db, IAuthenticationService auth);
+        void NavigateTo(string route, AppUser user, INavigationService nav, IDatabaseService databaseService, IAuthenticationService auth);
+
+        void NavigateTo(string route, AppContainer? container, AppUser user, INavigationService nav, IDatabaseService databaseService, IAuthenticationService auth);
+
         void GoBack();
     }
 
--- Services/NavigationServices/NavigationService.cs
+++ Services/NavigationServices/NavigationService.cs
@@ -1,43 +1,62 @@
 ﻿using System.Windows.Controls;
-using TFG.ViewModels;
+using TFG.Models;
+using TFG.Services.AuthentificationServices;
+using TFG.Services.DatabaseServices;
 using TFG.Views.Pages;
+using TFG.Views.Pages.Workspace.Container;
 using TFGDesktopApp.Models;
 
 namespace TFG.Services.NavigationServices {
-    public class NavigationService : INavigationService {
-        private readonly Frame _frame;
-
-        public NavigationService(Frame frame) {
-            _frame = frame;
-        }
-
-        public void NavigateToLogin() {
-            _frame.Navigate(new LoginPage(_frame));
-        }
-
-
-        public void NavigateTo(string route, AppUser user, INavigationService nav) {
-
-            switch (route) {
-
-                case "Workspace":
-                    _frame.Navigate(new WorkSpacePage(user, this));
-                    break;
-                case "Profile":
-                    _frame.Navigate(new UserProfilePage(user, this));
-                    break;
-                case "ProfileEdit":
-                    _frame.Navigate(new UserProfileEditPage(user, this));
-                    break;
-                case "ProfilePassword":
-                    _frame.Navigate(new UserProfileEditPasswordPage(user, this));
-                    break;
-            }
-        }
+    public class NavigationService(Frame frame) : INavigationService {
+        private readonly Frame _frame = frame;
         public void GoBack() {
             if (_frame.CanGoBack) {
                 _frame.GoBack();
             }
+        }
+        public void NavigateTo(IDatabaseService db, IAuthenticationService auth) {
+            _frame.Navigate(new LoginPage(this, db, auth));
+        }
+
+        public void NavigateTo(string route, AppUser user, INavigationService nav, IDatabaseService db, IAuthenticationService auth) {
+            switch (route) {
+                case "Workspace":
+                    _frame.Navigate(new WorkSpacePage(user, this, db, auth, null));
+                    break;
+                case "ProfileEdit":
+                    _frame.Navigate(new UserProfileEditPage(user, this, db, auth));
+                    break;
+                case "Profile":
+                    _frame.Navigate(new UserProfilePage(user, this, db, auth));
+                    break;
+                case "ProfilePassword":
+                    _frame.Navigate(new UserProfileEditPasswordPage(user, this, db, auth));
+                    break;
+            }
+
+        }
+
+        public void NavigateTo(string route, AppContainer? container, AppUser user, INavigationService nav, IDatabaseService db, IAuthenticationService auth) {
+
+            switch (route) {
+
+                case "Container":
+                    _frame.Navigate(new AppContainerPage(container, this, user, db, auth));
+                    break;
+                case "ContainerEdit":
+                    _frame.Navigate(new AppContainerCreateOrEditPage(container, user, this, db, auth));
+                    break;
+                case "ProfilePassword":
+                    _frame.Navigate(new UserProfileEditPasswordPage(user, this, db, auth));
+                    break;
+                case "Workspace":
+                    _frame.Navigate(new WorkSpacePage(user, this, db, auth, container)); // Pasamos el contenedor como parámetro
+                    break;
+                case "ContainerAdd":
+                    _frame.Navigate(new AppContainerCreateOrEditPage(container, user, this, db, auth));
+                    break;
+            }
+
         }
 
     }
