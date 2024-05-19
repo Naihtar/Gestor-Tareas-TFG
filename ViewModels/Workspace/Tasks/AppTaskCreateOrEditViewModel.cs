32eb205e774c9abc59b3de0918d77d758ca87bb5
Naihtar <sltrpa.no@gmail.com>
2024-05-19 20:32:03 +0200

    (CSP) - ViewModels/Workspace/Task: Implementación de nuevos métodos.
     - Modificado el viewModel para trabajar tanto en creación como edición.
     - Implementado el método para eliminar tareas.

---
+++ ViewModels/Workspace/Tasks/AppTaskCreateOrEditViewModel.cs
@@ -0,0 +1,70 @@
+﻿using TFG.Models;
+using TFG.Services.AuthentificationServices;
+using TFG.Services.DatabaseServices;
+using TFG.Services.NavigationServices;
+using TFG.ViewModels.Base;
+
+namespace TFG.ViewModels.Workspace.Tasks {
+    public class AppTaskCreateOrEditViewModel : AppTaskBaseViewModel {
+        public CommandViewModel SaveTaskCommand { get; }
+        private readonly bool _isCreate;
+        private readonly string? _status;
+
+        //public AppTaskCreateOrEditViewModel(AppTask task, AppContainer container, AppUser user, INavigationService navigationService, IDatabaseService db, IAuthenticationService auth)
+        //    : base(task, container, user, navigationService, db, auth) {
+        //    SaveTaskCommand = new CommandViewModel(async (obj) => await SaveTaskAsyncWrapper());
+        //    _isCreate = false;
+        //}
+        //public AppTaskCreateOrEditViewModel(AppTask? task, AppContainer container, AppUser user, INavigationService navigationService, IDatabaseService db, IAuthenticationService auth, string status)
+        //    : base(task, container, user, navigationService, db, auth) {
+        //    SaveTaskCommand = new CommandViewModel(async (obj) => await SaveTaskAsyncWrapper());
+        //    _status = status;
+        //    _isCreate = _appTask == null;
+        //}
+
+        public AppTaskCreateOrEditViewModel(AppTask? task, AppContainer container, AppUser user, INavigationService navigationService, IDatabaseService db, IAuthenticationService auth, string status = null)
+            : base(task, container, user, navigationService, db, auth) {
+            SaveTaskCommand = new CommandViewModel(async (obj) => await SaveTaskAsyncWrapper());
+            _status = status;
+            _isCreate = _appTask == null;
+        }
+
+        private bool CheckFieldName() {
+            return string.IsNullOrEmpty(EditableTask.NombreTarea);
+        }
+
+        protected override async Task SaveTaskAsyncWrapper() {
+            // Verificar que el campo de nombre no esté vacío
+            bool fieldName = CheckFieldName();
+
+            if (fieldName) {
+                ErrorMessage = "Rellene el campo del nombre.";
+                return;
+            }
+
+            // Verificar si alguna etiqueta excede los 16 caracteres
+            var invalidTags = EditableTask.EtiquetasTarea.Where(tag => tag.Length > 16).ToList();
+            if (invalidTags.Count != 0) {
+                ErrorMessage = $"La etiqueta '{invalidTags[0]}' excede los 16 caracteres.";
+                return;
+            }
+
+
+            if (EditableTask.EtiquetasTarea.Count() > 3) {
+                ErrorMessage = $"Has introducido más de 3 etiquetas.";
+                return;
+            }
+
+            if (_isCreate) {
+                EditableTask.ContenedorID = _appContainer.IdContenedor;
+                EditableTask.FechaCreacionTarea = DateTime.Now;
+                EditableTask.EstadoTarea = _status;
+                await SaveAddContainerAsync();
+                return;
+            }
+
+            // Guardar los cambios
+            await SaveEditChangesAsync();
+        }
+    }
+}
--- ViewModels/Workspace/Tasks/AppTaskViewModel.cs
+++ ViewModels/Workspace/Tasks/AppTaskViewModel.cs
@@ -9,10 +9,12 @@ namespace TFG.ViewModels.Workspace.Tasks {
     public class AppTaskViewModel : AppTaskBaseViewModel {
 
         public CommandViewModel EditTaskAccessCommand { get; }
+        public CommandViewModel DeleteTaskCommand { get; }
 
 
         public AppTaskViewModel(AppContainer? container, AppUser user, INavigationService navigationService, IDatabaseService db, IAuthenticationService auth, AppTask task) : base(task, container, user, navigationService, db, auth) {
             EditTaskAccessCommand = new CommandViewModel(EditTaskAccess);
+            DeleteTaskCommand = new CommandViewModel(async (obj) => await DeleteTask());
 
         }
 
@@ -20,7 +22,10 @@ namespace TFG.ViewModels.Workspace.Tasks {
             _navigationService.NavigateTo("EditTask", _appContainer, _user, _navigationService, _databaseService, _authenticationService, EditableTask);
 
         }
-
+        private async Task DeleteTask() {
+            await _databaseService.DeleteTask(EditableTask.IdTarea);
+            _navigationService.NavigateTo("Workspace", _appContainer, _user, _navigationService, _databaseService, _authenticationService);
+        }
         protected override Task SaveTaskAsyncWrapper() {
             return Task.CompletedTask;
         }
