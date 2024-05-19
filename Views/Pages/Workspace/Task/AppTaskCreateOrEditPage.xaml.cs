6ee8fe1424ecc172d6761590d2afcb292a2a13c1
Naihtar <sltrpa.no@gmail.com>
2024-05-19 20:40:46 +0200

    (---) Views/Pages/Workspace/Task: Cambios menores.

---
+++ Views/Pages/Workspace/Task/AppTaskCreateOrEditPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using TFG.Models;
+using TFG.Services.AuthentificationServices;
+using TFG.Services.DatabaseServices;
+using TFG.Services.NavigationServices;
+using TFG.ViewModels.Workspace.Tasks;
+
+namespace TFG.Views.Pages.Workspace.Task {
+    public partial class AppTaskCreateOrEditPage : Page {
+        public AppTaskCreateOrEditPage(AppContainer container, AppUser user, INavigationService nav, IDatabaseService db, IAuthenticationService auth, AppTask? task, string status = null) {
+            InitializeComponent();
+            DataContext = new AppTaskCreateOrEditViewModel(task, container, user, nav, db, auth, status);
+        }
+    }
+}
