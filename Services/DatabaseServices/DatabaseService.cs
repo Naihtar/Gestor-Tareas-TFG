15f610d8ba6a52c5118000af569a02cf6583ec56
Naihtar <sltrpa.no@gmail.com>
2024-05-22 17:19:43 +0200

    (CSP) - DatabaseServices: Reestructuración del código para una mejor lectura.

--- Services/DatabaseServices/DatabaseService.cs
+++ Services/DatabaseServices/DatabaseService.cs
@@ -7,146 +7,173 @@ using TFG.Models;
 namespace TFG.Services.DatabaseServices {
     public class DatabaseService(IDatabaseConnection connectionDB) : IDatabaseService {
 
-        //Atributos
         private readonly IDatabaseConnection _dataBase = connectionDB;
 
-        // Método genérico.
         public async Task<IMongoCollection<T>> GetCollectionAsync<T>(string collectionName) {
             return await Task.FromResult(_dataBase.GetCollection<T>(collectionName));
         }
 
-        // Método usuario.
+        // AppUser Methods
         public async Task<AppUser> GetUserByIdAsync(ObjectId userId) {
-            // Obtén la colección de usuarios de forma asíncrona
             var users = await GetCollectionAsync<AppUser>("usuarios");
-
-            // Busca al usuario en la colección de forma asíncrona
-            var user = await users.Find(userSearch => userSearch.IdUsuario == userId).FirstOrDefaultAsync();
-
-            return user;
+            return await users.Find(userSearch => userSearch.IdUsuario == userId).FirstOrDefaultAsync();
         }
 
-        public async Task UpdateUserAsync(AppUser user) {
-            // Obtén la colección de usuarios de forma asíncrona
+        public async Task<AppUser> GetUserByEmailAsync(string email) {
             var users = await GetCollectionAsync<AppUser>("usuarios");
+            return await users.Find(userSearch => userSearch.EmailUsuario == email).FirstOrDefaultAsync();
+        }
 
-            // Crea un filtro para encontrar al usuario por su ID
+        public async Task<bool> UpdateUserAsync(AppUser user) {
+            var users = await GetCollectionAsync<AppUser>("usuarios");
             var filter = Builders<AppUser>.Filter.Eq(userSearch => userSearch.IdUsuario, user.IdUsuario);
-
-            // Actualiza el usuario en la colección de forma asíncrona
-            await users.ReplaceOneAsync(filter, user);
+            var result = await users.ReplaceOneAsync(filter, user);
+            return result.IsAcknowledged;
         }
 
-        public async Task<bool> ExistEmailDB(ObjectId userId, string emailUsuario) {
+        public async Task<bool> CheckUsernameAsync(string userName) {
             var users = await GetCollectionAsync<AppUser>("usuarios");
+            var filter = Builders<AppUser>.Filter.Eq("aliasUsuario", userName);
+            return await users.Find(filter).AnyAsync();
+        }
 
-            // Crea un filtro para buscar un usuario con el mismo email pero diferente al usuario actual
+        public async Task<bool> CheckEmailAsync(string userEmail) {
+            var users = await GetCollectionAsync<AppUser>("usuarios");
+            var filter = Builders<AppUser>.Filter.Eq("email", userEmail);
+            return await users.Find(filter).AnyAsync();
+        }
+
+        public async Task<bool> CheckEmailByUserIDAsync(ObjectId userId, string emailUsuario) {
+            var users = await GetCollectionAsync<AppUser>("usuarios");
             var filter = Builders<AppUser>.Filter.And(
                 Builders<AppUser>.Filter.Eq("email", emailUsuario),
                 Builders<AppUser>.Filter.Ne("_id", userId)
             );
-
-            // Verifica si existe algún usuario que coincida con el filtro
             return await users.Find(filter).AnyAsync();
         }
-        public async Task<bool> ExistAliasDB(ObjectId userId, string aliasUsuario) {
-            var collection = await GetCollectionAsync<AppUser>("usuarios");
 
+        public async Task<bool> CheckUsernameByUserIDAsync(ObjectId userId, string aliasUsuario) {
+            var collection = await GetCollectionAsync<AppUser>("usuarios");
             var filter = Builders<AppUser>.Filter.And(
                 Builders<AppUser>.Filter.Eq("aliasUsuario", aliasUsuario),
                 Builders<AppUser>.Filter.Ne("_id", userId)
             );
-
             return await collection.Find(filter).AnyAsync();
         }
 
-        // Método usuario - Verificar password.
         public async Task<bool> VerifyPasswordAsync(ObjectId userId, string password) {
-
             var users = await GetCollectionAsync<AppUser>("usuarios");
-
             var user = await users.Find(user => user.IdUsuario == userId).FirstOrDefaultAsync();
-
-            if (BCrypt.Net.BCrypt.Verify(password, user.PasswordUsuario)) {
-                return true;
-            }
-            return false;
+            return user != null && BCrypt.Net.BCrypt.Verify(password, user.PasswordUsuario);
         }
 
         public async Task<List<ObjectId>> GetListContianerUser(ObjectId userId) {
-
             var collection = await GetCollectionAsync<AppUser>("usuarios");
-
             var user = await collection.Find(appUser => appUser.IdUsuario == userId).FirstOrDefaultAsync();
-
-            return user?.ListaContenedoresUsuario ?? [];
+            return user?.ListaContenedoresUsuario ?? new List<ObjectId>();
         }
 
-        // Metodo Contenedor:
+        public async Task<bool> CreateAppUser(AppUser user) {
+            var collection = await GetCollectionAsync<AppUser>("usuarios");
+            try {
+                await collection.InsertOneAsync(user);
+                return true;
+            } catch (Exception ex) {
+                MessageBox.Show($"Error al intentar crear la tarea. {ex.Message}");
+                return false;
+            }
+        }
 
+
+        public async Task<bool> DeleteAppUser(AppUser appUser) {
+            try {
+                var userCollection = await GetCollectionAsync<AppUser>("usuarios");
+                var containerCollection = await GetCollectionAsync<AppContainer>("contenedores");
+                var taskCollection = await GetCollectionAsync<AppTask>("tareas");
+
+                // Crear un filtro para encontrar todos los contenedores asociados al usuario
+                var containerFilter = Builders<AppContainer>.Filter.Eq(c => c.UsuarioID, appUser.IdUsuario);
+                var containers = await containerCollection.Find(containerFilter).ToListAsync();
+
+                // Borrado de tareas y contenedores
+                foreach (var container in containers) {
+                    var taskFilter = Builders<AppTask>.Filter.Eq(t => t.ContenedorID, container.IdContenedor);
+
+                    // Borramos las tareas asociadas al contenedor
+                    await taskCollection.DeleteManyAsync(taskFilter);
+
+                    // Borramos el contenedor
+                    await containerCollection.DeleteOneAsync(Builders<AppContainer>.Filter.Eq(c => c.IdContenedor, container.IdContenedor));
+                }
+
+                // Finalmente, borrar el usuario
+                var userFilter = Builders<AppUser>.Filter.Eq(u => u.IdUsuario, appUser.IdUsuario);
+                var userResult = await userCollection.DeleteOneAsync(userFilter);
+
+                return userResult.DeletedCount > 0;
+            } catch (Exception ex) {
+                MessageBox.Show($"Error al intentar eliminar el usuario y sus contenedores y tareas asociadas. {ex.Message}");
+                return false;
+            }
+        }
+
+        // AppContainer Methods
         public async Task<AppContainer> GetContainerByIdAsync(ObjectId containerId) {
             var collection = await GetCollectionAsync<AppContainer>("contenedores");
             var filter = Builders<AppContainer>.Filter.Eq(c => c.IdContenedor, containerId);
             return await collection.Find(filter).FirstOrDefaultAsync();
         }
 
-        public async Task UpdateContainerAsync(AppContainer container) {
-            // Obtener la colección de contenedores.
+        public async Task<bool> UpdateContainerAsync(AppContainer container) {
             var collection = await GetCollectionAsync<AppContainer>("contenedores");
-
-            // Crea un filtro para encontrar el contenedor deseado.
             var filter = Builders<AppContainer>.Filter.Eq(containerSearch => containerSearch.IdContenedor, container.IdContenedor);
-
-            // Actualizar el contenedor.
-            await collection.ReplaceOneAsync(filter, container);
+            var result = await collection.ReplaceOneAsync(filter, container);
+            return result.IsAcknowledged;
         }
 
         public async Task<bool> ExistContainerWithName(string nombreContenedor, ObjectId userId) {
             var collection = await GetCollectionAsync<AppContainer>("contenedores");
-
             var filter = Builders<AppContainer>.Filter.And(
                 Builders<AppContainer>.Filter.Eq("nombre", nombreContenedor),
                 Builders<AppContainer>.Filter.Eq("usuario_id", userId)
             );
-
             return await collection.Find(filter).AnyAsync();
         }
 
+        public async Task<bool> HasTasksInContainerAsync(ObjectId containerID) {
+            try {
+                var collection = await GetCollectionAsync<AppContainer>("contenedores");
+                var filter = Builders<AppContainer>.Filter.Eq(c => c.IdContenedor, containerID);
+                var container = await collection.Find(filter).FirstOrDefaultAsync();
+                return container != null && container.ListaTareas.Count > 0;
+            } catch (Exception ex) {
+                MessageBox.Show($"Error obtaining tasks from container: {ex.Message}");
+                return false;
+            }
+        }
+
+
+
+
         public async Task<bool> DeleteContainerAndTasks(ObjectId idContenedor, ObjectId userId) {
             try {
-                // Obtén la colección de contenedores
                 var containerCollection = await GetCollectionAsync<AppContainer>("contenedores");
-
-                // Crea un filtro para encontrar el contenedor
                 var containerFilter = Builders<AppContainer>.Filter.Eq(c => c.IdContenedor, idContenedor);
-
-                // Encuentra el contenedor
                 var container = await containerCollection.Find(containerFilter).FirstOrDefaultAsync();
 
                 if (container != null) {
-                    // Obtén la colección de tareas
                     var taskCollection = await GetCollectionAsync<AppTask>("tareas");
-
-                    // Crea un filtro para encontrar las tareas asociadas al contenedor
                     var taskFilter = Builders<AppTask>.Filter.Eq(t => t.ContenedorID, idContenedor);
-
-                    // Elimina todas las tareas asociadas al contenedor
                     var taskResult = await taskCollection.DeleteManyAsync(taskFilter);
 
-                    // Si se eliminaron las tareas o no había tareas asociadas, elimina el contenedor
                     if (taskResult.DeletedCount > 0 || taskResult.DeletedCount == 0) {
                         var containerResult = await containerCollection.DeleteOneAsync(containerFilter);
-
-                        // Si se elimina el contenedor, actualiza la lista de contenedores del usuario
                         if (containerResult.DeletedCount > 0) {
                             var userCollection = await GetCollectionAsync<AppUser>("usuarios");
                             var userFilter = Builders<AppUser>.Filter.Eq(u => u.IdUsuario, userId);
-
-                            // Remueve el ID del contenedor de la lista de contenedores del usuario
                             var userUpdate = Builders<AppUser>.Update.Pull(u => u.ListaContenedoresUsuario, idContenedor);
                             await userCollection.UpdateOneAsync(userFilter, userUpdate);
                         }
-
                         return containerResult.DeletedCount > 0;
                     }
                 }
@@ -157,9 +184,28 @@ namespace TFG.Services.DatabaseServices {
             }
         }
 
+        public async Task<bool> AddContainer(AppContainer container, ObjectId userId) {
+            var collection = await GetCollectionAsync<AppContainer>("contenedores");
+            var userCollection = await GetCollectionAsync<AppUser>("usuarios");
+            try {
+                await collection.InsertOneAsync(container);
+                var user = await userCollection.Find(u => u.IdUsuario == userId).FirstOrDefaultAsync();
 
-        // Metodo Tasks:
+                if (user != null) {
+                    user.ListaContenedoresUsuario.Add(container.IdContenedor);
+                    var update = Builders<AppUser>.Update.Set(appUser => appUser.ListaContenedoresUsuario, user.ListaContenedoresUsuario);
+                    var result = await userCollection.UpdateOneAsync(appUser => appUser.IdUsuario == userId, update);
+                    return result.IsAcknowledged;
+                }
 
+                return false;
+            } catch (Exception ex) {
+                MessageBox.Show($"Error al intentar crear el contenedor. {ex.Message}");
+                return false;
+            }
+        }
+
+        // AppTask Methods
         public async Task<AppTask> GetTastkByIdAsync(ObjectId taskId) {
             var collection = await GetCollectionAsync<AppTask>("tareas");
             var filter = Builders<AppTask>.Filter.Eq(t => t.IdTarea, taskId);
@@ -172,68 +218,28 @@ namespace TFG.Services.DatabaseServices {
             return await collection.Find(filter).ToListAsync();
         }
 
-        public async Task<bool> AddContainer(AppContainer container, ObjectId userId) {
-            var collection = await GetCollectionAsync<AppContainer>("contenedores");
-            var userCollection = await GetCollectionAsync<AppUser>("usuarios");
-
-            try {
-                await collection.InsertOneAsync(container);
-
-                // Obtén el usuario de la base de datos
-                var user = await userCollection.Find(u => u.IdUsuario == userId).FirstOrDefaultAsync();
-
-                if (user != null) {
-                    // Agrega el ID del contenedor a la lista de contenedores del usuario
-                    user.ListaContenedoresUsuario.Add(container.IdContenedor);
-
-                    // Actualiza el usuario en la base de datos
-                    var update = Builders<AppUser>.Update.Set(appUser => appUser.ListaContenedoresUsuario, user.ListaContenedoresUsuario);
-                    await userCollection.UpdateOneAsync(appUser => appUser.IdUsuario == userId, update);
-                }
-
-                return true;
-            } catch (Exception ex) {
-                MessageBox.Show($"Error al intentar crear el contenedor. {ex.Message}");
-                return false;
-            }
-        }
-
-        public async Task UpdateTaskAsync(AppTask task) {
+        public async Task<bool> UpdateTaskAsync(AppTask task) {
             var tasks = await GetCollectionAsync<AppTask>("tareas");
             var filter = Builders<AppTask>.Filter.Eq(tarea => tarea.IdTarea, task.IdTarea);
-            await tasks.ReplaceOneAsync(filter, task);
+            var result = await tasks.ReplaceOneAsync(filter, task);
+            return result.IsAcknowledged;
         }
 
         public async Task<bool> DeleteTask(ObjectId idTarea) {
             try {
-                // Obtén la colección de tareas
                 var taskCollection = await GetCollectionAsync<AppTask>("tareas");
-
-                // Crea un filtro para encontrar la tarea
                 var taskFilter = Builders<AppTask>.Filter.Eq(t => t.IdTarea, idTarea);
-
-                // Encuentra la tarea
                 var task = await taskCollection.Find(taskFilter).FirstOrDefaultAsync();
 
                 if (task != null) {
-                    // Obtén la colección de contenedores
                     var containerCollection = await GetCollectionAsync<AppContainer>("contenedores");
-
-                    // Crea un filtro para encontrar los contenedores asociados a la tarea
                     var containerFilter = Builders<AppContainer>.Filter.AnyEq(container => container.ListaTareas, idTarea);
-
-                    // Encuentra los contenedores
                     var containers = await containerCollection.Find(containerFilter).ToListAsync();
-
-                    // Remueve el ID de la tarea de la lista de tareas de cada contenedor
                     var containerUpdate = Builders<AppContainer>.Update.Pull(container => container.ListaTareas, idTarea);
                     foreach (var container in containers) {
                         await containerCollection.UpdateOneAsync(containerFilter, containerUpdate);
                     }
-
-                    // Elimina la tarea
                     var taskResult = await taskCollection.DeleteOneAsync(taskFilter);
-
                     return taskResult.DeletedCount > 0;
                 }
                 return false;
@@ -249,25 +255,20 @@ namespace TFG.Services.DatabaseServices {
 
             try {
                 await collection.InsertOneAsync(appTask);
-
-                // Obtén el usuario de la base de datos
                 var container = await containerCollection.Find(container => container.IdContenedor == containerId).FirstOrDefaultAsync();
 
                 if (container != null) {
-                    // Agrega el ID del contenedor a la lista de contenedores del usuario
                     container.ListaTareas.Add(appTask.IdTarea);
-
-                    // Actualiza el usuario en la base de datos
                     var update = Builders<AppContainer>.Update.Set(appContainer => appContainer.ListaTareas, container.ListaTareas);
-                    await containerCollection.UpdateOneAsync(appContainer => appContainer.IdContenedor == containerId, update);
+                    var result = await containerCollection.UpdateOneAsync(appContainer => appContainer.IdContenedor == containerId, update);
+                    return result.IsAcknowledged;
                 }
 
-                return true;
+                return false;
             } catch (Exception ex) {
                 MessageBox.Show($"Error al intentar crear la tarea. {ex.Message}");
                 return false;
             }
         }
-
     }
 }
--- Services/DatabaseServices/IDatabaseService.cs
+++ Services/DatabaseServices/IDatabaseService.cs
@@ -4,22 +4,35 @@ using TFG.Models;
 
 namespace TFG.Services.DatabaseServices {
     public interface IDatabaseService {
+        // Funciones genéricas
         Task<IMongoCollection<T>> GetCollectionAsync<T>(string collectionName);
+
+        // Funciones para AppUser
         Task<AppUser> GetUserByIdAsync(ObjectId userId);
-        Task UpdateUserAsync(AppUser user);
-        Task<bool> ExistEmailDB(ObjectId userId, string emailUsuario);
-        Task<bool> ExistAliasDB(ObjectId userId, string aliasUsuario);
+        Task<AppUser> GetUserByEmailAsync(string email);
+        Task<bool> UpdateUserAsync(AppUser user);
+        Task<bool> CheckEmailByUserIDAsync(ObjectId userId, string emailUsuario);
+        Task<bool> CheckUsernameByUserIDAsync(ObjectId userId, string aliasUsuario);
         Task<bool> VerifyPasswordAsync(ObjectId userId, string password);
+        Task<bool> CheckEmailAsync(string email);
+        Task<bool> CheckUsernameAsync(string username);
+        Task<bool> CreateAppUser(AppUser user);
+        Task<bool> DeleteAppUser(AppUser appUser);
+
+        // Funciones para AppContainer
         Task<AppContainer> GetContainerByIdAsync(ObjectId containerId);
-        Task UpdateContainerAsync(AppContainer container);
+        Task<bool> UpdateContainerAsync(AppContainer container);
         Task<bool> ExistContainerWithName(string nombreContenedor, ObjectId userId);
-        Task<AppTask> GetTastkByIdAsync(ObjectId taskId);
-        Task<List<AppTask>> GetTasksByContainerIdAsync(ObjectId containerId);
         Task<bool> DeleteContainerAndTasks(ObjectId containerId, ObjectId userId);
         Task<bool> AddContainer(AppContainer container, ObjectId userId);
         Task<List<ObjectId>> GetListContianerUser(ObjectId userId);
-        Task UpdateTaskAsync(AppTask task);
+        Task<bool> HasTasksInContainerAsync(ObjectId containerID);
+        // Funciones para AppTask
+        Task<AppTask> GetTastkByIdAsync(ObjectId taskId);
+        Task<List<AppTask>> GetTasksByContainerIdAsync(ObjectId containerId);
+        Task<bool> UpdateTaskAsync(AppTask task);
         Task<bool> DeleteTask(ObjectId taskId);
         Task<bool> AddTask(AppTask task, ObjectId containerId);
     }
 }
+
