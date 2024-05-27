4ffc4016da78ef2e15cda1c3ea776fad3cea2d75
Naihtar <sltrpa.no@gmail.com>
2024-05-27 08:57:12 +0200

    (CSP) - NavigationServices: Diversos cambios
    - Reorganizadas las rutas para una mayor claridad
    - Añadidos comentarios
    - Implementados los mensajes de "success"

--- Services/NavigationServices/INavigationService.cs
+++ Services/NavigationServices/INavigationService.cs
@@ -1,20 +1,16 @@
-﻿using System.ComponentModel;
-using TFG.Models;
-using TFG.Services.AuthentificationServices;
-using TFG.Services.DatabaseServices;
-using TFG.Models;
+﻿using TFG.Models;
 
 namespace TFG.Services.NavigationServices {
 
     public interface INavigationService {
-        void NavigateTo(string route ,IDatabaseService db, IAuthenticationService auth);
-        void NavigateTo(string route, AppUser user, INavigationService nav, IDatabaseService databaseService, IAuthenticationService auth);
-
-        void NavigateTo(string route, AppContainer? container, AppUser user, INavigationService nav, IDatabaseService databaseService, IAuthenticationService auth);
-        void NavigateTo(string route, AppContainer? container, AppUser user, INavigationService nav, IDatabaseService databaseService, IAuthenticationService auth, AppTask?task);
-        void NavigateTo(string route, AppContainer? container, AppUser user, INavigationService nav, IDatabaseService databaseService, IAuthenticationService auth, AppTask? task, string status);
-
         void GoBack();
+        void NavigateTo();
+        void NavigateTo(string? successMessage);
+        void NavigateTo(string route, AppUser appUser);
+        void NavigateTo(string route, AppUser appUser, AppContainer? appContainer);
+        void NavigateTo(AppUser appUser, AppContainer? appContainer, AppTask? appTask);
+        void NavigateTo(AppUser appUser, AppContainer? appContainer, string? successMessage);
+        void NavigateTo(string route, AppUser appUser, AppContainer? appContainer, AppTask? appTask, string? data);
     }
 
 }
--- Services/NavigationServices/NavigationService.cs
+++ Services/NavigationServices/NavigationService.cs
@@ -9,91 +9,101 @@ using TFG.Views.Pages.Workspace.Container;
 using TFG.Views.Pages.Workspace.Task;
 
 namespace TFG.Services.NavigationServices {
-    public class NavigationService(Frame frame) : INavigationService {
-        private readonly Frame _frame = frame;
+    public class NavigationService(Frame frame, IDatabaseService databaseService, IAuthenticationService authenticationService) : INavigationService {
+        private readonly Frame _frame = frame; //Marco principal, donde cargaran las vistas.
+        private readonly IDatabaseService _databaseService = databaseService; //Dependencia de los servicios de la base de datos
+        private readonly IAuthenticationService _authenticationService = authenticationService; //Dependencia de los servicios de autentificación
+
         public void GoBack() {
+            //Volver para atrás
             if (_frame.CanGoBack) {
                 _frame.GoBack();
             }
         }
-        public void NavigateTo(string route, IDatabaseService db, IAuthenticationService auth) {
-            switch (route) {
-                case "LogIn":
-                    _frame.Navigate(new LogInPage(this, db, auth));
-                    break;
-                case "SignUp":
-                    _frame.Navigate(new SignUpPage(db, this, auth));
-                    break;
-            }
+
+        //Rutas:
+        public void NavigateTo() {
+            //Sign Up
+            _frame.Navigate(new SignUpPage(_databaseService, _authenticationService, this));
+        }
+
+        public void NavigateTo(string? successMessage) {
+            //Log In
+            _frame.Navigate(new LogInPage(_authenticationService, this, successMessage));
         }
 
 
-        public void NavigateTo(string route, AppUser user, INavigationService nav, IDatabaseService db, IAuthenticationService auth) {
+        // Editar Perfil y contraseña
+        public void NavigateTo(string route, AppUser appUser) {
             switch (route) {
-                case "Workspace":
-                    _frame.Navigate(new WorkSpacePage(user, this, db, auth, null));
-                    break;
-
-                case "ProfilePassword":
-                    _frame.Navigate(new UserProfileEditPasswordPage(user, this, db, auth));
-                    break;
-            }
-
-        }
-
-        public void NavigateTo(string route, AppContainer? container, AppUser user, INavigationService nav, IDatabaseService db, IAuthenticationService auth) {
-
-
-            switch (route) {
-
-                case "Container":
-                    _frame.Navigate(new AppContainerPage(container, this, user, db, auth));
-                    break;
-                case "ContainerEdit":
-                    _frame.Navigate(new AppContainerCreateOrEditPage(container, user, this, db, auth));
-                    break;
-                case "ProfilePassword":
-                    _frame.Navigate(new UserProfileEditPasswordPage(user, this, db, auth));
-                    break;
-                case "Workspace":
-                    _frame.Navigate(new WorkSpacePage(user, this, db, auth, container)); // Pasamos el contenedor como parámetro
-                    break;
-                case "ContainerAdd":
-                    _frame.Navigate(new AppContainerCreateOrEditPage(container, user, this, db, auth));
-                    break;
-            }
-
-        }
-
-
-        public void NavigateTo(string route, AppContainer? container, AppUser user, INavigationService nav, IDatabaseService db, IAuthenticationService auth, AppTask? task) {
-            switch (route) {
-                case "Task":
-                    _frame.Navigate(new AppTaskPage(container, user, this, db, auth, task));
-                    break;
-                case "EditTask":
-                    _frame.Navigate(new AppTaskCreateOrEditPage(container, user, this, db, auth, task));
-                    break;
-                case "Profile":
-                    _frame.Navigate(new UserProfilePage(user, this, db, auth, task, container));
-                    break;
-                case "DeleteProfile":
-                    _frame.Navigate(new UserProfileDeletePage(user, this, db, auth, task, container));
-                    break;
                 case "ProfileEdit":
-                    _frame.Navigate(new UserProfileEditPage(user, this, db, auth, task, container));
+                    _frame.Navigate(new UserProfileEditPage(_databaseService, this, appUser: appUser));
+                    break;
+                case "ProfilePassword":
+                    _frame.Navigate(new UserProfileEditPasswordPage(_databaseService, _authenticationService, this, appUser: appUser));
+                    break;
+                default:
+                    _frame.Navigate(new UserProfileEditPage(_databaseService, this, appUser: appUser));
                     break;
             }
         }
-        public void NavigateTo(string route, AppContainer? container, AppUser user, INavigationService nav, IDatabaseService db, IAuthenticationService auth, AppTask? task, string status) {
+
+        public void NavigateTo(string route, AppUser appUser, AppContainer? appContainer) {
             switch (route) {
-                case "AddTask":
-                    _frame.Navigate(new AppTaskCreateOrEditPage(container, user, this, db, auth, null, status));
+                //Información de espacio de trabajo
+                case "Container":
+                    _frame.Navigate(new AppContainerPage(_databaseService, this, appUser: appUser, appContainer: appContainer));
+                    break;
+                //Editar el espacio de trabajo
+                case "ContainerEdit":
+                    _frame.Navigate(new AppContainerCreateOrEditPage(_databaseService, this, appUser: appUser, appContainer: appContainer));
+                    break;
+                //Añadir un nuevo espacio de trabajo
+                case "ContainerAdd":
+                    _frame.Navigate(new AppContainerCreateOrEditPage(_databaseService, this, appUser: appUser, appContainer: null));
+                    break;
+                default:
+                    //En caso de error te retorna a la información del espacio de trabajo.
+                    _frame.Navigate(new AppContainerPage(_databaseService, this, appUser: appUser, appContainer: appContainer));
                     break;
             }
         }
 
+        public void NavigateTo(AppUser appUser, AppContainer? appContainer, AppTask? appTask) {
+            //Eliminar el perfil de usuario
+            _frame.Navigate(new UserProfileDeletePage(_databaseService, _authenticationService, this, appUser: appUser, appContainer: appContainer, appTask: appTask));
+        }
 
+        public void NavigateTo(AppUser appUser, AppContainer? appContainer, string? successMessage) {
+            //Espacio de trabajo
+            _frame.Navigate(new WorkSpacePage(_databaseService, this, appUser: appUser, appContainer: appContainer, successMessage));
+        }
+
+        public void NavigateTo(string route, AppUser appUser, AppContainer? appContainer, AppTask? appTask, string? data) {
+            switch (route) {
+                //Información sobre la tarea seleccionada
+                case "Task":
+                    _frame.Navigate(new AppTaskPage(_databaseService, this, appUser: appUser, appContainer: appContainer, appTask: appTask));
+                    break;
+                //Editar una tarea
+                case "TaskEdit":
+                    _frame.Navigate(new AppTaskCreateOrEditPage(_databaseService, this, appUser: appUser, appContainer: appContainer, appTask: appTask, null));
+                    break;
+                //Añadir una tarea
+                case "TaskAdd":
+                    _frame.Navigate(new AppTaskCreateOrEditPage(_databaseService, this, appUser: appUser, appContainer: appContainer, appTask: null, data));
+                    break;
+                //Información sobre el usuario
+                case "Profile":
+                    _frame.Navigate(new UserProfilePage(_databaseService, this, appUser: appUser, appContainer: appContainer, appTask: appTask, data));
+                    break;
+                default:
+                    //En caso de error te retorna al perfil
+                    _frame.Navigate(new UserProfilePage(_databaseService, this, appUser: appUser, appContainer: appContainer, appTask: appTask, data));
+                    break;
+
+            }
+        }
     }
 }
 
