5e606849ef853263d79939fcc001434a26e52860
Naihtar <sltrpa.no@gmail.com>
2024-05-28 11:50:09 +0200

    (CSP) - MainWindowViewModel: Implementación de carga de lenguaje.

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,21 @@
-﻿using TFG.Services.NavigationServices;
+﻿using TFG.Services;
+using TFG.Services.NavigationServices;
 using TFG.ViewModels.Base;
 
 namespace TFG.ViewModels {
     public class MainWindowViewModel : BaseViewModel {
 
+        //Dependencias
         private readonly INavigationService _navigationService;
+        private readonly ILocalizationService _localizationService;
 
-        public MainWindowViewModel(INavigationService navigationService) {
+        public MainWindowViewModel(INavigationService navigationService, ILocalizationService localizationService) {
             _navigationService = navigationService;
+            _localizationService = localizationService;
+
+            string languageCode = _localizationService.GetLanguage(); //Obtenemos el idioma por defecto de la app.
+            _localizationService.SetLanguage(languageCode); // Cargar el idioma de la app
+
             _navigationService.NavigateTo(null);
         }
     }
