55262cb69862fdb65e61285c089467fd0dc9d5fa
Naihtar <sltrpa.no@gmail.com>
2024-05-28 11:49:33 +0200

    (CSP) - LogIgViewModel: Diversos cambios
     - Implementación de la nueva dependencia de servicios.
     - Implementada la lógica y el comando para ejecutar los cambios de idiomas.

--- ViewModels/Access/LoginViewModel.cs
+++ ViewModels/Access/LoginViewModel.cs
@@ -1,9 +1,8 @@
-﻿using System.Windows;
-using TFG.Services.AuthentificationServices;
-using TFG.Services.DatabaseServices;
+﻿using TFG.Services.AuthentificationServices;
 using TFG.Services.NavigationServices;
 using TFG.ViewModels.Base;
 using TFG.Models;
+using TFG.Services;
 
 namespace TFG.ViewModels {
     public class LogInViewModel : BaseViewModel {
@@ -11,15 +10,16 @@ namespace TFG.ViewModels {
         //Dependencias
         private readonly IAuthenticationService _authenticationService; //Dependencia de los servicios de autentificación
         private readonly INavigationService _navigationService; //Dependencia de los servicios navegación
+        private readonly ILocalizationService _localizationService; //Dependencia de los servicios de traducción de la app
 
         //Atributos
         private AppUser? _appUser;
         private string? _data; //Corresponde al correo electrónico o nombre de usuario
         public string? Data {
 
-            get { return _data; }
+            get { return _data.ToLower(); }
             set {
-                _data = value;
+                _data = value.ToLower();
                 OnPropertyChanged(Data); //Notifica al programa que la variable Data ha sido modificada
             }
         }
@@ -31,22 +31,30 @@ namespace TFG.ViewModels {
                 _password = value; OnPropertyChanged(Password); //Notifica al programa que la variable Data ha sido modificada
             }
         }
+
+        private readonly string _actualLanguage;
+
         //Comandos
         public CommandViewModel LogInCommand { get; private set; } //Comando para acceder a tu cuenta
         public CommandViewModel SignUpCommand { get; private set; } //Comando para crear una cuenta
+        public CommandViewModel ChangeLanguageCommand { get; private set; } //Cambiar idioma
 
         //Constructor
-        public LogInViewModel(IAuthenticationService authenticationService, INavigationService navigationService, string? successMessage) {
+        public LogInViewModel(IAuthenticationService authenticationService, INavigationService navigationService, ILocalizationService localizationService, string? successMessage) {
             _navigationService = navigationService;
             _authenticationService = authenticationService;
+            _localizationService = localizationService;
 
             Data = string.Empty;
             Password = string.Empty;
             Message = successMessage;
+            _actualLanguage = _localizationService.GetLanguage() ?? "es-ES";
 
             LogInCommand = new CommandViewModel(async (obj) => await LoginAsync(), CanLogin);
             SignUpCommand = new CommandViewModel(SignUpAccess);
+            ChangeLanguageCommand = new CommandViewModel(ChangeLanguage);
 
+            DisableLanguageBtns(_actualLanguage);
             if (Message != null) {
                 //Muestra mensaje informativo tras completar una acción, como por ejemplo cerrar la sesión.
                 ShowSuccessMessage(Message);
@@ -64,7 +72,7 @@ namespace TFG.ViewModels {
         // Método para autenticar al usuario
         private async Task LoginAsync() {
             //Comprueba que el usuario existe en la base de datos
-            bool isAuthenticated = await _authenticationService.AuthenticateUserAsync(Data, Password);
+            bool isAuthenticated = await _authenticationService.AuthenticateUserAsync(input: Data, password: Password);
             if (!isAuthenticated) {
                 SuccessOpen = false;
                 ErrorOpen = true;
@@ -74,7 +82,7 @@ namespace TFG.ViewModels {
             }
 
             //Obtiene el usuario através del correo electrónico o nombre de usuario
-            _appUser = await _authenticationService.GetUserByDataInputAsync(Data);
+            _appUser = await _authenticationService.GetUserByDataInputAsync(input: Data);
             SuccessOpen = false;
             if (_appUser == null) {
                 ErrorOpen = true;
@@ -91,6 +99,19 @@ namespace TFG.ViewModels {
             _navigationService.NavigateTo(); //Carga la vista de creación de cuentas
         }
 
+        private void ChangeLanguage(object obj) {
 
+            string? lang = obj as string;
+
+            lang ??= "es-ES"; //En caso de que status sea "null" se agregara automáticamente a "es-ES"
+
+            DisableLanguageBtns(lang);
+
+            _localizationService.SetLanguage(lang);
+
+            string? msg = ResourceDictionary["SuccessLanguageChangeInfoBarStr"] as string;
+
+            _navigationService.NavigateTo(msg);
+        }
     }
 }
