7accea4d0ef89e31e8b7a7398676052664f18c12
Naihtar <sltrpa.no@gmail.com>
2024-05-29 16:54:04 +0200

    (---) Cambios menores en las vista WorkspacePage

--- Views/MainWindowView.xaml.cs
+++ Views/MainWindowView.xaml.cs
@@ -11,12 +11,24 @@ namespace TFG {
     public partial class MainWindowView : FluentWindow {
         public MainWindowView() {
             InitializeComponent();
+
+            //Inicialización de las dependencias
+
+            //Obteniendo la clave de conexión
             IConnectionManager connectionManager = new ConnectionManager();
+            
+            //Conectandose a la base de datos
             IDatabaseConnection databaseConnection = new DatabaseConnection(connectionManager);
             IDatabaseService databaseService = new DatabaseService(databaseConnection);
             IAuthenticationService authenticationService = new AuthenticationService(databaseService);
+
+            //Iniciando la traducción predefinida o de la sesión anterior
             ILocalizationService localizationService = new LocalizationService();
+
+            //Iniciando el servicio de navegación junto a las dependencias inicializadas.
             INavigationService navigationService = new NavigationService(MainFrame, databaseService, authenticationService, localizationService);
+            
+            //Asignando el viewmodel al contexto de la vista junto a llas dependencias que usara.
             DataContext = new MainWindowViewModel(navigationService, localizationService);
 
             //Fijar tamaño pantalla al iniciar.
